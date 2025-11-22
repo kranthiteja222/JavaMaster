99e8dbfb2bfcdd7284866da949b38f622acc7d15
kranthi <kranthiteja222@gmail.com>
2025-11-22 20:49:12 +0530

    type casting

---
+++ src/com/kranthiPra/TypeCasting.java
@@ -0,0 +1,18 @@
+package com.kranthiPra;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        //converting one data type to another like Flot to Int
+        // we can do this when two types are compatible like (Int and float) it should not be (chat and int or flot)
+        //if the size is small then no need to do type casting ex : float can convert to Int without type casting
+        int a = (int)(10.278f);
+        System.out.println(a);
+
+        float b= 10;
+        System.out.println(b);// here no need to type cast
+
+
+
+
+    }
+}
