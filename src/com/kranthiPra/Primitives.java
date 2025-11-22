1c8e5b645dfe1842160543ed733056ffa324a005
kranthi <kranthiteja222@gmail.com>
2025-11-22 15:58:36 +0530

    new topic primitive

---
+++ src/com/kranthiPra/Primitives.java
@@ -0,0 +1,21 @@
+package com.kranthiPra;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno =64;
+        char a='a';
+        float b=3.14f; //here we need to give 'f' at the end  because by default type is double
+        double c=71387838.33910; // double is to store bigger value
+        long longInt = 123456789099303L;  //here we need to give 'L' for longInt because by default it is Int
+        boolean bool = true;
+
+
+        //string is not primitive because we can break string in small parts
+        //example string to Char like Teja - T e j a
+        String name="Kranthi";
+
+
+
+    }
+
+}
