e49cac6d245040ceb6301c6a18e1c893142db2a5
kranthi <kranthiteja222@gmail.com>
2025-11-22 17:06:55 +0530

    new files for input & Sum

---
+++ src/com/kranthiPra/Inputs.java
@@ -0,0 +1,19 @@
+package com.kranthiPra;
+
+import java.util.Scanner;
+
+public class Inputs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your rollno");
+        int rollNo = sc.nextInt();
+        System.out.println(rollNo);
+        sc.nextLine();
+        System.out.println("enter your name");
+        String name=sc.nextLine();
+
+        System.out.println(name);
+
+    }
+}
